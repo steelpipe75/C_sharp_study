c529760fdd6d52b53fa9cca4c7450c1ce79bc5e2
Nishizaki <steelpipe75@gmail.com>
2023-06-05 22:55:58 +0900

    切断処理見直し、各操作キーを追加

--- WpfGamepadApp/Controller.cs
+++ WpfGamepadApp/Controller.cs
@@ -12,21 +12,49 @@ namespace WpfGamepadApp
     internal class Controller
     {
         private IXbox360Controller controller;
+        private bool continue_flag;
+        private Task procTask;
 
         public Controller()
         {
             var client = new ViGEmClient();
             controller = client.CreateXbox360Controller();
+            procTask = null;
+            continue_flag = false;
+        }
+
+        ~Controller()
+        {
+            Stop();
         }
 
         public void Start()
         {
+            continue_flag = true;
+            procTask = new Task(Proc);
+            procTask.Start();
+        }
+
+        public void Proc()
+        {
             controller.Connect();
+
+            while (continue_flag)
+            {
+
+            }
+
+            controller.Disconnect();
         }
 
         public void Stop()
         {
-            controller.Disconnect();
+            continue_flag = false;
+            if (procTask != null)
+            {
+                procTask.Wait();
+            }
+            procTask = null;
         }
     }
 }
